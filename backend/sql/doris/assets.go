38b4829218fc4252018e2e61fafc9c8b83b3bcb3
Bobo <yanglinbo@gmail.com>
2026-03-16 20:26:54 +0800

    feat: refactor Doris schema into modular SQL files; add Kafka and aggregate table definitions; update asset embedding and config for gw_uba database

--- backend/sql/doris/assets.go
+++ backend/sql/doris/assets.go
@@ -3,4 +3,16 @@ package doris
 import _ "embed"
 
 //go:embed 1_base_tables.sql
-var SchemaData []byte
+var BaseTablesData []byte
+
+//go:embed 02_kafka_tables.sql
+var KafkaTablesData []byte
+
+//go:embed 03_aggregate_tables.sql
+var AggregateTablesData []byte
+
+//go:embed 04_indexes.sql
+var IndexesData []byte
+
+//go:embed 05_views.sql
+var ViewsData []byte
